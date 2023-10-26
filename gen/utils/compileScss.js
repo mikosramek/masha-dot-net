6a5ed5d2642b6f7bd4fc88893511e7b7e5f75386
mikosramek <miko@mikosramek.ca>
2023-10-26 18:11:49 -0400

    feat: compile Sass step

--- gen/utils/compileScss.js
+++ gen/utils/compileScss.js
@@ -1,16 +1,24 @@
 const path = require("path");
-
+const fs = require("fs");
 const sass = require("node-sass");
+
+const outFile = path.resolve(__dirname, "..", "static", "styles.css");
 
 const compileSass = () => {
   return new Promise((resolve, reject) => {
     sass.render(
       {
         includePaths: [path.resolve(__dirname, "..", "styles")],
+        file: path.resolve(__dirname, "..", "styles", "styles.scss"),
+        outFile,
       },
-      (err) => {
-        if (err) return reject(err);
-        resolve();
+      (renderError, result) => {
+        if (renderError) return reject(renderError);
+
+        fs.writeFile(outFile, result.css, (writeError) => {
+          if (writeError) return reject(writeError);
+          resolve();
+        });
       }
     );
   });
