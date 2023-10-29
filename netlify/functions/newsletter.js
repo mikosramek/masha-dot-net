b800f76f3912f8551b883234545e685acac42c23
mikosramek <miko@mikosramek.ca>
2023-10-29 16:32:11 -0400

    feat: base functions

--- netlify/functions/newsletter.js
+++ netlify/functions/newsletter.js
@@ -1,8 +1,18 @@
+// send out newsletter
 export const handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello World! eee",
-    }),
-  };
+  try {
+    // const body = await attemptMail();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "correct" }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "error",
+        error,
+      }),
+    };
+  }
 };
---
+++ netlify/functions/signup.js
@@ -0,0 +1,10 @@
+// signup to newsletter
+
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "WIP: allow email to be used",
+    }),
+  };
+};
